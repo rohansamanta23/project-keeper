29e8fd4df3b4b1a178082b099fe9173a87c2a9e3
rohan samanta <rohansamanta45@gmail.com>
2024-02-29 23:40:30 +0530

    form page

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from './Header';
+import Page from './Page';
+function App(){
+    return <>
+        <Header/>
+        <Page/>
+    </>;
+}
+export default App;
