4d463db6a1734bd41c49666b6a72184f634dbcec
rohan samanta <rohansamanta45@gmail.com>
2024-03-01 22:48:35 +0530

    note page added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from './Header';
 import Page from './Page';
+import Note from './Note';
+
 function App(){
+    var [loader,loadState] = useState(false);
+
+    function changeState(){
+        loader?loadState(false):loadState(true);
+    }
+
     return <>
         <Header/>
-        <Page/>
+        {loader?<div className="notes-section">
+            <Note/><Note/><Note/><Note/><Note/><Note/><Note/><Note/><Note/><Note/><Note/><Note/>
+        </div>
+        :<Page/>}
+        
     </>;
 }
 export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Notes(){
+    return (<div className="note-set">
+        <h3 className="note-heading">Tital</h3>
+        <p className="note-contant">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, eligendi nemo dolore, maxime omnis dolorum rerum a doloribus ipsam quis nihil excepturi possimus culpa voluptate laboriosam adipisci facilis at magnam.</p>
+    </div>);
+}
+
+export default Notes;
